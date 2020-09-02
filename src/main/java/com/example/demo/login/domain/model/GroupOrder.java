4a47e57c790eb77cf78ae33633bd5073f6dd05bf
gorilla <log1230gori@outlook.jp>
2020-09-02 18:08:43 +0900

    commit 6 end

--- src/main/java/com/example/demo/login/domain/model/GroupOrder.java
+++ src/main/java/com/example/demo/login/domain/model/GroupOrder.java
@@ -2,7 +2,8 @@ package com.example.demo.login.domain.model;
 
 import javax.validation.GroupSequence;
 
-@GroupSequence({ValidGroup1.class,
+@GroupSequence({
+        ValidGroup1.class,
         ValidGroup2.class,
         ValidGroup3.class})
 public interface GroupOrder {
--- src/main/java/com/example/demo/login/domain/model/SignupForm.java
+++ src/main/java/com/example/demo/login/domain/model/SignupForm.java
@@ -9,40 +9,32 @@ import java.util.Date;
 
 @Data
 public class SignupForm {
-    //入力必須、メールアドレス形式
-    //@NotBlank(message = "{require_check}")
-    //@Email(message = "{email_check}")
-    @NotBlank(groups = ValidGroup1.class)
-    @Email(groups = ValidGroup2.class)
-    private String userId;
+    //必須入力、メールアドレス形式
+    @NotBlank(groups = ValidGroup1.class, message = "{require_check}")
+    @Email(groups = ValidGroup2.class, message = "{email_check}")
+    private String userId; // ユーザーID
 
     //必須入力、長さ4から100桁まで、半角英数字のみ
-    //@NotBlank(message = "{require_check}")
-    @NotBlank(groups = ValidGroup1.class)
-    //@Length(min = 4, max = 100, message = "{length_check}")
-    @Length(min=4, max=100, groups = ValidGroup2.class)
-
-    //@Pattern(regexp = "^[a-zA-Z0-9]+$", message = "{pattern_check}")
-    @Pattern(regexp = "^[a-zA-Z0-9]+$",groups = ValidGroup3.class)
-    private String password;
+    @NotBlank(groups = ValidGroup1.class, message = "{require_check}")
+    @Length(groups = ValidGroup2.class, message = "{length_check}")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", groups = ValidGroup3.class, message = "{pattern_check}")
+    private String password; // パスワード
 
     //必須入力
-    //@NotBlank(message = "{require_check}")
-    @NotBlank(groups = ValidGroup1.class)
-    public String userName;
+    @NotBlank(groups = ValidGroup1.class, message = "{require_check}")
+    private String userName; // ユーザー名
 
     //必須入力
-    //@NotNull(message = "{require_check}")
-    @NotNull(groups = ValidGroup1.class)
-    @DateTimeFormat(pattern="yyyy/MM/dd")
-    private Date birthday;
+    @NotNull(groups = ValidGroup1.class, message = "{require_check}")
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private Date birthday; // 誕生日
 
     //値が20から100まで
-    @Min(value = 20,groups = ValidGroup2.class)
-    @Max(value = 100, groups = ValidGroup2.class)
-    private int age;
+    @Min(value = 20, groups = ValidGroup2.class, message = "{min_check}")
+    @Max(value = 100, groups = ValidGroup2.class, message = "{max_check}")
+    private int age; // 年齢
 
     //falseのみ可能
-    @AssertFalse(groups = ValidGroup2.class)
-    private boolean marriage;
+    @AssertFalse(groups = ValidGroup2.class, message = "{false_check}")
+    private boolean marriage; // 結婚ステータス
 }
